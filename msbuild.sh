502d0292c13877f3276cc076a7447ab6f8725533
Ivaylo <ivaylo5ev@gmail.com>
2017-08-28 15:31:53 +0300

    updated readme, added msbuild and paket wrappers

---
+++ msbuild.sh
@@ -0,0 +1,12 @@
+# A cross-platform wrapper around the MSBuild functionality
+if [ -z "$msbuild" ]; then
+  if [ "$(uname)" == "Darwin" ]; then
+    msbuild='MSBuild'
+  elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    msbuild="xbuild";
+  else
+    msbuild="\"/C/Program Files (x86)/Microsoft Visual Studio/2017/Community/MSBuild/15.0/Bin/MSBuild.exe\"";
+  fi
+fi
+
+eval "${msbuild} \"$@\""
